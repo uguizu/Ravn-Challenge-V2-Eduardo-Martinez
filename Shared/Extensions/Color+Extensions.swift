4003068a59da70626e18ec3f3f0d177dca2bd91d
Developer <eduardo.martinez@hugoapp.com>
2021-02-23 21:06:14 -0600

    feat: Add extensions for color and image

---
+++ Shared/Extensions/Color+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Extensions.swift
+//  Ravn-Challenge-V2 (iOS)
+//
+//  Created by emartinez on 2/23/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let textDark = Color("textDark")
+    static let textLight = Color("textLight")
+}
