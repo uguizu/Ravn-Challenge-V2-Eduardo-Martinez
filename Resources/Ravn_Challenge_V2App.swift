613a5e6329019e871cb59e88c785fd79064a21fd
Developer <eduardo.martinez@hugoapp.com>
2021-02-23 21:31:12 -0600

    feat: Add custom navigation bar configuration.

--- Resources/Ravn_Challenge_V2App.swift
+++ Resources/Ravn_Challenge_V2App.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct Ravn_Challenge_V2App: App {
+    
+    init() {
+        let coloredAppearance = UINavigationBarAppearance()
+        coloredAppearance.configureWithOpaqueBackground()
+        coloredAppearance.backgroundColor = .black
+        coloredAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        UINavigationBar.appearance().standardAppearance = coloredAppearance
+        UINavigationBar.appearance().compactAppearance = coloredAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PeopleListView()
         }
     }
 }
