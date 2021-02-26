32c21b0ee0c3191df3326494c3eb708512677e55
Developer <eduardo.martinez@hugoapp.com>
2021-02-25 18:39:29 -0600

    refactor: Refactor configuration for navigation controller.

--- Resources/Ravn_Challenge_V2App.swift
+++ Resources/Ravn_Challenge_V2App.swift
@@ -9,22 +9,6 @@ import SwiftUI
 
 @main
 struct Ravn_Challenge_V2App: App {
-    
-    init() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = .ravnBlack
-        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        let backImageIndicator = UIImage.iconBack
-        appearance.setBackIndicatorImage(backImageIndicator, transitionMaskImage: backImageIndicator)
-        
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().compactAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
-        UINavigationBar.appearance().tintColor = .white
-    }
-    
     var body: some Scene {
         WindowGroup {
             PeopleListView()
--- Shared/Extensions/NavigationViewController+Extensions.swift
+++ Shared/Extensions/NavigationViewController+Extensions.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 extension UINavigationController: UINavigationControllerDelegate {
-    // MARK: Variables Declaration
-    open override var preferredStatusBarStyle: UIStatusBarStyle {
-        traitCollection.userInterfaceStyle == .dark ? .darkContent : .lightContent
-    }
-    
     // MARK: Lifecycle
     open override func viewDidLoad() {
         super.viewDidLoad()
+        applyStyle()
         delegate = self
+    }
+    
+    // MARK: Privaye methods
+    private func applyStyle() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .ravnBlack
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.adaptativeWhite ?? .white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.adaptativeWhite ?? .white]
+        let backImageIndicator = UIImage.iconBack
+        appearance.setBackIndicatorImage(backImageIndicator, transitionMaskImage: backImageIndicator)
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().tintColor = .adaptativeWhite
     }
     
     // MARK: UINavigationControllerDelegate methods
